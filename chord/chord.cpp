b9a7ca3502d18d7d4c0665746098677096cc5c60
Joe Beda <joe.github@bedafamily.com>
2020-11-01 14:27:31 -0800

    Refactor and class-ify main app
    Signed-off-by: Joe Beda <joe.github@bedafamily.com>

--- chord/chord.cpp
+++ chord/chord.cpp
@@ -11,27 +11,58 @@
 using namespace daisy;
 using namespace daisysp;
 
-DaisyPatch patch;
+DaisyPatch gPatch;
 
-// osc_st[3] is the fundamental semitone
-float      fund_st;
-float      osc_st[3];
-Oscillator osc[3];
+class ChordApp {
+ public:
+  void        Init(DaisyPatch *_patch);
+  static void StaticAudioCallback(float **in, float **out, size_t size);
+  void        AudioCallback(float **in, float **out, size_t size);
+  void        UpdateControls();
+  void        UpdateOled();
 
-std::string waveNames[5];
+  void Run();
 
-int waveform;
-int final_wave;
+  float ReadControlInV(int i) { return ReadCalibratedCV(patch, i); }
 
-const int   base_note      = 24;  // C1
-const float voltage_offset = semitone_to_e1vo(base_note);
+  const int   base_note      = 24;  // C1
+  const float voltage_offset = semitone_to_e1vo(base_note);
+  const char *waveNames[5]   = {"sine", "triangle", "saw", "ramp", "square"};
+  const int   final_wave     = Oscillator::WAVE_POLYBLEP_TRI;
 
-// Are we currently calibrating?
-bool calibrating = false;
+  DaisyPatch *patch;
 
-void UpdateControls();
+  int        waveform;
+  float      fund_st;    // Fundamental frequency in semitones
+  float      osc_st[3];  // Semitone freq for derived oscillators
+  Oscillator osc[3];
+};
 
-static void AudioCallback(float **in, float **out, size_t size) {
+ChordApp app;
+
+void ChordApp::Init(DaisyPatch *_patch) {
+  patch    = _patch;
+  waveform = 0;
+
+  auto samplerate = patch->AudioSampleRate();
+  for (int i = 0; i < 3; i++) {
+    osc[i].Init(samplerate);
+    osc[i].SetAmp(.7);
+  }
+}
+
+void ChordApp::Run() {
+  patch->StartAudio(StaticAudioCallback);
+
+  while (1) {
+    UpdateOled();
+  }
+}
+void ChordApp::StaticAudioCallback(float **in, float **out, size_t size) {
+  app.AudioCallback(in, out, size);
+}
+
+void ChordApp::AudioCallback(float **in, float **out, size_t size) {
   UpdateControls();
   for (size_t i = 0; i < size; i++) {
     float mix = 0;
@@ -47,84 +78,42 @@ static void AudioCallback(float **in, float **out, size_t size) {
   }
 }
 
-float ReadControlInV(int i) {
-  return ReadCalibratedCV(&patch, i);
-}
+void ChordApp::UpdateOled() {
+  patch->display.Fill(false);
 
-void SetupOsc(float samplerate) {
-  for (int i = 0; i < 3; i++) {
-    osc[i].Init(samplerate);
-    osc[i].SetAmp(.7);
-  }
-}
+  patch->display.SetCursor(0, 0);
+  patch->display.WriteString("0xBEDA Chord", Font_6x8, true);
 
-void SetupWaveNames() {
-  waveNames[0] = "sine";
-  waveNames[1] = "triangle";
-  waveNames[2] = "saw";
-  waveNames[3] = "ramp";
-  waveNames[4] = "square";
-}
+  patch->display.SetCursor(0, 10);
+  patch->display.WriteString("waveform: ", Font_6x8, true);
+  patch->display.WriteString(waveNames[waveform], Font_7x10, true);
 
-void UpdateOled();
-
-int main(void) {
-  patch.Init();  // Initialize hardware (daisy seed, and patch)
-  patch.StartAdc();
-
-  RunCalibration(&patch);
-
-  LoadSettings(&patch.seed);
-
-  waveform   = 0;
-  final_wave = Oscillator::WAVE_POLYBLEP_TRI;
-
-  SetupOsc(patch.AudioSampleRate());
-  SetupWaveNames();
-  patch.StartAudio(AudioCallback);
-
-  while (1) {
-    UpdateOled();
-  }
-}
-
-void UpdateOled() {
-  patch.display.Fill(false);
-
-  patch.display.SetCursor(0, 0);
-  patch.display.WriteString("0xBEDA Chord", Font_6x8, true);
-
-  patch.display.SetCursor(0, 10);
-  patch.display.WriteString("waveform: ", Font_6x8, true);
-  patch.display.WriteString(waveNames[waveform].c_str(), Font_7x10, true);
-
-  patch.display.SetCursor(0, 20);
+  patch->display.SetCursor(0, 20);
   char        val[50];
   std::string note_name = semitone_name(fund_st);
   snprintf(val, 50, "Fund: %0.0f Hz %s", semitone_to_hz(fund_st),
            note_name.c_str());
-  patch.display.WriteString(val, Font_6x8, true);
+  patch->display.WriteString(val, Font_6x8, true);
 
-  patch.display.SetCursor(0, 30);
+  patch->display.SetCursor(0, 30);
   snprintf(val, 50, "%4s %4s %4s", semitone_name(osc_st[0]).c_str(),
            semitone_name(osc_st[1]).c_str(), semitone_name(osc_st[2]).c_str());
-  patch.display.WriteString(val, Font_6x8, true);
+  patch->display.WriteString(val, Font_6x8, true);
 
-  patch.display.Update();
+  patch->display.Update();
 }
 
-void UpdateControls() {
-  patch.DebounceControls();
-  patch.UpdateAnalogControls();
+void ChordApp::UpdateControls() {
+  patch->DebounceControls();
+  patch->UpdateAnalogControls();
 
   // encoder
-  waveform += patch.encoder.Increment();
+  waveform += patch->encoder.Increment();
   waveform = (waveform % final_wave + final_wave) % final_wave;
 
   // Figure out the fundamental frequency
-  float fund_e1vo = ReadControlInV(3) +
-                    voltage_offset;
-  fund_st = e1vo_to_qsemitone(fund_e1vo);
+  float fund_e1vo = ReadControlInV(3) + voltage_offset;
+  fund_st         = e1vo_to_qsemitone(fund_e1vo);
 
   // Tune each osc
   for (int i = 0; i < 3; i++) {
@@ -134,4 +123,16 @@ void UpdateControls() {
     osc[i].SetFreq(osc_hz);
     osc[i].SetWaveform((uint8_t)waveform);
   }
+}
+
+int main(void) {
+  gPatch.Init();  // Initialize hardware (daisy seed, and patch)
+  gPatch.StartAdc();
+
+  RunCalibration(&gPatch);
+
+  LoadSettings(&gPatch.seed);
+
+  app.Init(&gPatch);
+  app.Run();
 }
